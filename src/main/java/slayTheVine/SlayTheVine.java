43caceb5e70c2025696622a77126e0ba2b9cef15
zach <tl2dot0@gmail.com>
2020-07-23 15:26:33 -0400

    .

--- src/main/java/slayTheVine/SlayTheVine.java
+++ src/main/java/slayTheVine/SlayTheVine.java
@@ -1,7 +1,32 @@
 package slayTheVine;
 
+import basemod.BaseMod;
+import basemod.interfaces.*;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import org.apache.logging.log4j.LogManager;
+import slayTheVine.cards.*;
+
+import java.util.logging.Logger;
 
 @SpireInitializer
-public class SlayTheVine {
+public class SlayTheVine implements EditCardsSubscriber {
+
+    public static final Logger logger = (Logger) LogManager.getLogger(SlayTheVine.class.getName());
+
+    public SlayTheVine() {
+        logger.info("Subscribing...");
+        BaseMod.subscribe(this);
+    }
+
+    @SuppressWarnings("unused")
+    public static void initialize() {
+        logger.info("initializing Slay The Vine; please hold...");
+        SlayTheVine slayTheVine = new SlayTheVine();
+    }
+
+    @Override
+    public void receiveEditCards() {
+        logger.info("Adding cards...");
+        BaseMod.addCard(new ZipZoom());
+    }
 }
